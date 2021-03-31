c3813fccb3d1013b5d24511fa0dca508f739de2f
Kaai Suzuki <suzydp@me.com>
2021-03-31 01:35:03 -0700

    Fetch data from fake json data

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      spots: [
-        {
-          name: 'andarsia',
-          id: 'and1'
-        },
-        {
-          name: 'antelope',
-          id: 'ant1'
-        },
-        {
-          name: 'everest',
-          id: 'evr1'
-        },
-      ]
+      spots: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({ spots:users }))
   }
 
   render() {
     return (
       <div className="App">
         {
-          this.state.spots.map(spot => <p key={spot.id}>{ spot.name }</p>)
+          this.state.spots.map(spot => <p key={spot.id}>{ spot.address.city }</p>)
         }
       </div>
     )
