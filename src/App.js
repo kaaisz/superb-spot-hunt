9928fe4c7fafb4a91ef136d1485738a086c1eb31
Kaai Suzuki <suzydp@me.com>
2021-03-31 00:55:50 -0700

    Check if setState is working in component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Kaai'
+    };
+  }
+
   render() {
     return (
-      <div>Hi</div>
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.string}</p>
+          <button onClick={()=>this.setState({ string: 'Hello Suzuki' })}>Change text</button>
+        </header>
+      </div>
     )
   }
 }
